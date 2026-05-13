ef4e71dcb17e41f6cdd8a8fb54d040a74a630651
luochiioi <raymand068@qq.com>
2026-05-13 11:26:44 +0800

    feat: 新增商品和商品图片实体类 Item & ItemImage

---
+++ backend/src/main/java/com/example/backend/entity/Item.java
@@ -0,0 +1,29 @@
+package com.example.backend.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@TableName("item")
+public class Item {
+    @TableId(type = IdType.AUTO)
+    private long id;
+    private long userId;
+    private String title;
+    private String campus;
+    private String conditionLevel;
+    private String description;
+    private BigDecimal price;
+    private long categoryId;
+    private String status;
+    private long viewCount;
+    private long favoriteCount;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private Integer isDeleted;
+}
---
+++ backend/src/main/java/com/example/backend/entity/ItemImage.java
@@ -0,0 +1,19 @@
+package com.example.backend.entity;
+
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("item_image")
+public class ItemImage {
+    private Long id;
+    private Long itemId;
+    private String imageUrl;
+    private Long sortOrder;
+    private LocalDateTime createTime;
+    private Integer isDeleted;
+
+}
